6d0467950f4e6915b81b97a6321e6f94ae5cf7e6
Matvey <morozovmatvej75@gmail.com>
2022-12-27 16:55:02 +0300

    first node app

---
+++ server.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount >= 10) {
+    console.log("large number");
+} else {
+    console.log("small number");
+}
+
+console.log("this is my first node application! Hello world!");
