5a441a38465adff139bbeda637fc2ae33a7a2b61
Matvey <morozovmatvej75@gmail.com>
2023-01-03 19:31:37 +0300

    POST request (form validation)

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.get("/api/people", (req, res) => {
     res.send(JSON.stringify({ ok: true, data: people }, null, 2))
 })
 
+app.use(express.urlencoded({ extended: false }));
+app.post("/login", (req, res) => {
+    const { name } = req.body;
+
+    if (name) {
+        res.status(200);
+        res.send(`Welcome, ${name}`)
+    }
+    else {
+        res.status(401);
+        res.send("Please provide a name");
+    }
+})
+
+
 app.listen(5000);
