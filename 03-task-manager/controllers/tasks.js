62b7762d3d7ac3bb6ada4acb4060d87357ff60ec
Matvey <morozovmatvej75@gmail.com>
2023-01-07 14:14:25 +0300

    get all tasks

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,7 +1,31 @@
 const Task = require("../models/Task");
 
-function getTasks(req, res) {
-    res.send("All tasks");    
+// Model.deleteMany()
+// Model.deleteOne()
+// Model.find()
+// Model.findById()
+// Model.findByIdAndDelete()
+// Model.findByIdAndRemove()
+// Model.findByIdAndUpdate()
+// Model.findOne()
+// Model.findOneAndDelete()
+// Model.findOneAndRemove()
+// Model.findOneAndReplace()
+// Model.findOneAndUpdate()
+// Model.replaceOne()
+// Model.updateMany()
+// Model.updateOne()
+
+
+async function getTasks(req, res) {
+    try {
+        const tasks = await Task.find({});
+        res.status(200);
+        res.json({ tasks });
+    } catch (err) {
+        res.status(500);
+        res.json({ err });
+    }
 }
 
 function getTask(req, res) {
