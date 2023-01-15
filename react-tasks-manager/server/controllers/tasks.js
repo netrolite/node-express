cf0e0a3be8c755d60131b4476bdd5332e419e81e
Matvey <morozovmatvej75@gmail.com>
2023-01-15 14:58:22 +0300

    create and send custom errors

--- react-tasks-manager/server/controllers/tasks.js
+++ react-tasks-manager/server/controllers/tasks.js
@@ -2,6 +2,8 @@
 const Task = require("../models/Task");
 // async wrapper so I don't have to use trycatch
 const async = require("../middleware/asyncWrapper");
+// custom API error
+const ApiError = require("../errors/ApiError");
 
 
 const getAllTasks = async(async (req, res) => {
@@ -14,23 +16,36 @@ const postTask = async(async (req, res) => {
     res.status(201).json(task);
 })
 
-const getTask = async(async (req, res) => {
+const getTask = async(async (req, res, next) => {
     const task = await Task.findOne({ _id: req.params.taskId });
+
+    if (!task) return next(new ApiError("Not found", 404));
+
     res.status(200).json(task);
 })
 
-const patchTask = async(async (req, res) => {
+const patchTask = async(async (req, res, next) => {
     // replace only the provided values
     const task = await Task.findOneAndUpdate(
         { _id: req.params.taskId },
         req.body,
         { new: true } // return updated document
     )
+    
+    console.log(task);
+    if (!task) {
+        console.log("task is falsy");
+        return next(new ApiError("Not found", 404));
+    }
+
     res.status(200).json(task);
 })
 
-const deleteTask = async(async (req, res) => {
+const deleteTask = async(async (req, res, next) => {
     const task = await Task.findOneAndDelete({ _id: req.params.taskId });
+
+    if (!task) return next(new ApiError("Not found", 404));
+    console.log("delete task");
     res.status(200).json(task);
 })
 
--- react-tasks-manager/server/middleware/errorHandler.js
+++ react-tasks-manager/server/middleware/errorHandler.js
@@ -1,4 +1,10 @@
+const ApiError = require("../errors/ApiError");
+
 function errorHandler(err, req, res, next) {
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({ message: err.message });
+    }
+
     console.log("ERROR!!!");
     // console.error(err);
     res.status(500).json(err);
