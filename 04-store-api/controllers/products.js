9a903303da2a1a742423ca6494cdfbc3fd8d40dd
Matvey <morozovmatvej75@gmail.com>
2023-01-16 20:11:41 +0300

    pagination

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -14,7 +14,7 @@ async function getAllProductsTesting(req ,res) {
 }
 
 async function getAllProducts(req, res) {
-    const { name, featured, company, price, rating, sort } = req.query;
+    const { name, featured, company, price, rating, sort, fields } = req.query;
 
     // using custom query to be able to do things like using regex or sorting with some properties
     const query = {};
@@ -33,18 +33,33 @@ async function getAllProducts(req, res) {
     }
 
     // this is async. Not using await to be able to use sorting function (event loop doesn't run this right away)
-    console.log("query:", query);
     let result = Product.find(query);
 
     if (sort) {
-        const sortString = sort.replace(/\,/, " ");
-        console.log(sortString);
+        // replace commans with spaces (sort function requires this syntax)
+        const sortString = sort.replaceAll(",", " ");
         result = result.sort(sortString);
     }
     else result = result.sort("createdAt");
 
-    const products = await result;
+    // select only specific fields
+    if (fields) {
+        const selectString = fields.replaceAll(",", " ");
+        result = result.select(selectString);
+    }
 
+    // pagination
+    // if results = 30, limit = 10, page = 2, then skip = 10 (shows 10 results)
+    // if results = 35, limit = 10, page = 3, then skip = 30 (shows 5 results)
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit
+
+    result = result
+        .skip(skip)
+        .limit(limit);
+
+    const products = await result;
     res.status(200).json({ resultsAmount: products.length, products });
 }
 
