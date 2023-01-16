b0e41944c15ecd9a75e666ed5b4ec832fae39445
Matvey <morozovmatvej75@gmail.com>
2023-01-16 16:53:26 +0300

    use regex for search queries

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,9 +1,12 @@
 const Product = require("../models/Product");
 
 async function getAllProductsTesting(req ,res) {
-    const query = "q";
+    const query = "d";
     const products = await Product.find({
-        name: { $regex: query }
+        name: {
+            $regex: query,
+            $options: "i"
+        }
     })
     res.status(200).json({ resultsAmount: products.length, products });
 }
@@ -11,16 +14,22 @@ async function getAllProductsTesting(req ,res) {
 async function getAllProducts(req, res) {
     const { name, featured, company, price, rating } = req.query;
 
-    // using queryObj so user can't use properties that don't exist
-    let queryObj = {};
-    // mongoose automatically converts string to boolean. Only accepts true and false. Everything else triggers an error
-    if (name) queryObj.name = name;
-    if (featured) queryObj.featured = featured; 
-    if (company) queryObj.company = company;
-    if (price) queryObj.price = price;
-    if (rating) queryObj.rating = rating;
+    // using custom query so user can't use properties that don't exist
+    let query = {};
 
-    const products = await Product.find(queryObj);
+    if (name) {
+        query.name = {
+            $regex: name,
+            $options: "i" // case insensitive
+        }
+    }
+    // mongoose automatically casts string to boolean. Only accepts "true" and "false". Everything else throws an error
+    if (featured) query.featured = featured; 
+    if (company) query.company = company;
+    if (price) query.price = price;
+    if (rating) query.rating = rating;
+
+    const products = await Product.find(query);
     res.status(200).json({ resultsAmount: products.length, products });
 }
 
