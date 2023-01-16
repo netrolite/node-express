2d2fe64bcff01d97fde97e11e5353f57ac7aa24a
Matvey <morozovmatvej75@gmail.com>
2023-01-16 19:09:19 +0300

    sorting

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,35 +1,50 @@
 const Product = require("../models/Product");
 
 async function getAllProductsTesting(req ,res) {
-    const query = "d";
+    const { name, featured, company, price, rating } = req.query;
+
     const products = await Product.find({
         name: {
-            $regex: query,
+            $regex: req.query.name,
             $options: "i"
         }
     })
+    .sort("name");
     res.status(200).json({ resultsAmount: products.length, products });
 }
 
 async function getAllProducts(req, res) {
-    const { name, featured, company, price, rating } = req.query;
+    const { name, featured, company, price, rating, sort } = req.query;
 
-    // using custom query so user can't use properties that don't exist
-    let query = {};
+    // using custom query to be able to do things like using regex or sorting with some properties
+    const query = {};
 
+    if (featured) query.featured = featured;
+    if (company) query.company = company;
+    if (price) query.price = price;
+    if (rating) query.rating = rating;
+
+    // modifying name property to use regex
     if (name) {
         query.name = {
             $regex: name,
             $options: "i" // case insensitive
         }
     }
-    // mongoose automatically casts string to boolean. Only accepts "true" and "false". Everything else throws an error
-    if (featured) query.featured = featured; 
-    if (company) query.company = company;
-    if (price) query.price = price;
-    if (rating) query.rating = rating;
 
-    const products = await Product.find(query);
+    // this is async. Not using await to be able to use sorting function (event loop doesn't run this right away)
+    console.log("query:", query);
+    let result = Product.find(query);
+
+    if (sort) {
+        const sortString = sort.replace(/\,/, " ");
+        console.log(sortString);
+        result = result.sort(sortString);
+    }
+    else result = result.sort("createdAt");
+
+    const products = await result;
+
     res.status(200).json({ resultsAmount: products.length, products });
 }
 
