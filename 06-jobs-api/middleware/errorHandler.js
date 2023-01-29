3fb8908773520da435582387bb3d798352043c4d
Matvey <morozovmatvej75@gmail.com>
2023-01-29 11:36:18 +0300

    handle duplicates (like email)

--- 06-jobs-api/middleware/errorHandler.js
+++ 06-jobs-api/middleware/errorHandler.js
@@ -1,16 +1,33 @@
+const { BadRequestError } = require("../errors");
 const ApiError = require("../errors/ApiError");
 
 function errorHandler(err, req, res, next) {
-    console.log(err);
+    const errObject = {
+        statusCode: err.statusCode,
+        message: err.message
+    }
 
-    let statusCode = err.statusCode;
-    let message = err.message;
+    console.log(errObject);
 
+    // if is a custom error like "BadRequestError" or "NotFoundError"
     if (err instanceof ApiError) {
         return res.status(statusCode).json({ message })
     }
 
-    return res.status(500).json({ err })
+    if (err.code === 11000) {
+        handleDuplicate(errObject, err.keyValue);
+        res.status(400).json(errObject);
+    }
+
+    return res.status(500).json(errObject);
 }
+
+
+function handleDuplicate(errObject, keyValue) {
+    errObject.message = "1 or more entries already exist"
+    const duplicateKeys = Object.keys(keyValue);
+    errObject.duplicateEntries = duplicateKeys;
+}
+
 
 module.exports = errorHandler
